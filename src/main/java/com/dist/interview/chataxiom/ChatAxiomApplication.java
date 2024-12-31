2e83ef8a8b6ff8ee745cf570c56ae48d0c66e900
Rajett <rajet.stage@gmail.com>
2024-12-31 03:53:24 +0100

    Initial commit

---
+++ src/main/java/com/dist/interview/chataxiom/ChatAxiomApplication.java
@@ -0,0 +1,13 @@
+package com.dist.interview.chataxiom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAxiomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAxiomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dist/interview/chataxiom/ChatAxiomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dist.interview.chataxiom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAxiomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
